de3453b8bb671ebf70b89153f5900d96fdc66dcb
lhern207 <lhern207@fiu.edu>
2017-10-13 19:46:36 -0400

    Add files via upload
    V2

--- TakeMySpot Server/database_connect.py
+++ TakeMySpot Server/database_connect.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python
 import mysql.connector as mariadb
 
-mariadb_connection = mariadb.connect(user='lhern207', password='Fastball99', database='takemyspotdb')
+mariadb_connection = mariadb.connect( user='root', password='Fastball99', database='takemyspotdb')
 cursor = mariadb_connection.cursor()
 
 #retrieving information
 some_name = 'Bob Marley'
-cursor.execute("SELECT vehicle FROM accounts WHERE name=%s", (some_name,))
 
-print "Vehicle: ", cursor.vehicle
+try:
+    cursor.execute("SELECT Vehicle, ID FROM accounts WHERE Name=%s", (some_name,))
+except mariadb.Error as error:
+    print("Error: {}".format(error))
 
-#for first_name, last_name in cursor:
-#    print("First name: {}, Last name: {}").format(first_name,last_name)
+
+for Vehicle, ID in cursor:
+    print("Vehicle: {}, ID: {}").format(Vehicle,ID)
 
 #insert information
 try:
-    cursor.execute("INSERT INTO accounts (ID, name, vehicle) VALUES (%d,%s,%s)", (4, 'Maria Test', 'Lexus ES350'))
+    cursor.execute("INSERT INTO accounts (Name, Vehicle) VALUES (%s,%s)", ('Maria Test', 'Lexus ES350'))
 except mariadb.Error as error:
     print("Error: {}".format(error))
 
 mariadb_connection.commit()
-print "The last inserted id was: ", cursor.lastrowid
+print ("The last inserted id was: ", cursor.lastrowid)
 
 mariadb_connection.close()
