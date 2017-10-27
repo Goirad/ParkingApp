545b83d37da3488b16082b81c91eeb402ba299f4
Dario Gonzalez <goiradio1@gmail.com>
2017-10-26 23:22:28 -0400

    Major refactoring

--- TakeMySpot Server/server.py
+++ TakeMySpot Server/server.py
@@ -12,12 +12,19 @@ HOST = ''
 PORT = 27182
 BUFSIZE = 2048
 
-queue = []
+leavingQueue = []
+parkingQueue = []
+
 socks = []  # socket
 conns = {}  # {userID: user}
-sockAddr = {}  # {socket: (host, port)}
+sockAddr = {}  # {socket: user}
 # we need two things
 # a list of (socket) which stores order
+
+expectedJSONArgs = {
+    'create' : ['userID', 'name', 'vehicle'],
+    'default': ['userID']
+}
 
 current_milli_time = lambda: int(round(time.time() * 1000))
 
@@ -26,199 +33,211 @@ def parse(raw_req):
     return json.loads(raw_req)
 
 
-def makeError(err):
+def makeError(self, err):
     print("error", err)
     return (404, json.dumps({'success': False, 'description': err}))
 
 
-def handleConnect(addr, req):
-    if len(req) != 1 or 'userID' not in req:
-        return makeError('Invalid command arguments')
+def checkArgs(self, reqJSON, fun):
+    if reqJSON.keys != expectedJSONArgs[fun]:
+        return True
     else:
-        userID = req['userID']
-        if userID in conns:
-            return makeError('User already connected')
-        else:
-            try:
-                user = User(userID, addr)
-                conns[userID] = user
-                return (200, json.dumps({'success': True, 'name': user.name, 'vehicle': user.vehicle}))
-            except Exception as e:
-                print(e)
-                return makeError('User not found')
+        return self.makeError('Invalid arguments for ' + fun)
 
+class Server:
 
-def handleDisconnect(addr, req):
-    if len(req) != 1 or 'userID' not in req:
-        return makeError('Invalid command arguments')
-    else:
-        userID = req['userID']
-        if userID in conns:
-            del conns[userID]
-            return (200, json.dumps({'success': True}))
-        else:
-            return makeError('User not connected')
-
-
-def handleCreate(req):
-    if len(req) != 3 or 'userID' not in req or 'name' not in req or 'vehicle' not in req:
-        return makeError('Invalid command arguments')
-    else:
-        dbFile = open('data.txt', 'r')
-        db = json.load(dbFile)
-        if req['userID'] in db['users']:
-            return makeError('User already exists')
-        else:
+    def handleConnect(self, addr, req):
+        res = checkArgs(req, 'default')
+        if res == True:
             userID = req['userID']
-            vehicle = req['vehicle']
-            name = req['name']
-
-            db['users'][userID] = {'vehicle': vehicle, 'name': name}
-
-            new = open('data1.txt', 'w')
-
-            json.dump(db, new, indent=4)
-
-            dbFile.close()
-            new.close()
-
-            os.rename('data.txt', 'data2.txt')
-            os.rename('data1.txt', 'data.txt')
-            os.rename('data2.txt', 'data1.txt')
-
-            return (200, json.dumps({'success': True}))
-
-
-def handlePark(req):
-    if len(req) != 1 or 'userID' not in req:
-        return makeError('Invalid command arguments')
-    else:
-        userID = req['userID']
-        if userID not in conns:
-            return makeError('User not connected')
-        else:
-            user = conns[userID]
-            user.lastActive = current_milli_time()
-            user.parking = True
-            queue.append(user)
-
-            return (200, json.dumps({'success': True}))
-
-
-def handleLeave(req):
-    if len(req) != 2 or 'userID' not in req or 'locationDescription' not in req:
-        return makeError('Invalid command arguments')
-    else:
-        userID = req['userID']
-        if userID not in conns:
-            return makeError('User not connected')
-        else:
-            user = conns[userID]
-            user.lastActive = current_milli_time()
-            if len(queue) == 0:
-                return makeError('No one wants to park')
+            if userID in conns:
+                return makeError('User already connected')
             else:
-                nextInLine = queue[0]
-                nextInLine.matched = True
-                nextInLine.matchUserID = userID
-                nextInLine.matchLocation = req['locationDescription']
-                return (200, json.dumps({'success': True, 'matchUserID': nextInLine.userID}))
-
-
-def handleAccept(req):
-    return
-
-
-def handleDecline(req):
-    return
-
-
-def handlePoll(req):
-    if len(req) != 1 or 'userID' not in req:
-        return makeError('Invalid command arguments')
-    else:
-        userID = req['userID']
-        if userID not in conns:
-            return makeError('User not connected')
+                try:
+                    user = User(userID, addr, self)
+                    conns[userID] = user
+                    return (200, json.dumps({'success': True, 'name': user.name, 'vehicle': user.vehicle}))
+                except Exception as e:
+                    print(e)
+                    return makeError('User not found')
         else:
-            user = conns[userID]
-            user.lastActive = current_milli_time()
-            reply = {'userID': userID, 'inQueue': user.inQueue}
-            if user.inQueue:
-                reply['matched'] = user.matched
-                reply['position'] = user.matchLocation
-                if user.matched:
-                    reply['matchUserID'] = user.matchUserID
-                    reply['matchLocation'] = user.matchLocation
+            return res
 
 
-# TODO check user is connected here not in each request method
-def handleRequest(addr, rawReq, page):
-    try:
-        req = parse(rawReq)
-    except Exception as e:
-        print(e)
-        return makeError('Invalid JSON')
 
-    if page == '/connect':
-        return handleConnect(addr, req)
-    elif page == '/create':
-        return handleCreate(req)
-    else:
-        return makeError('Invalid command')
-
-
-# Once all of last second's requests have been processed, do all the logic
-def doLogic():
-    for user in conns:
-        if time.time() - conns[user].lastActive > 30:
-            del conns[user]
-    return
-
-
-serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serverSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-try:
-    serverSock.bind((HOST, PORT))
-except socket.error as msg:
-    print('Bind failed. Error Code : ' + str(msg))
-
-serverSock.listen(10)
-socks.append(serverSock)
-
-# Main Loop
-
-while True:
-    start = current_milli_time()
-
-    readSockets, writeSockets, errorSockets = select.select(socks, [], [], .01)
-
-    for socket in readSockets:
-        if socket == serverSock:
-            newSock, newAddr = serverSock.accept()
-            socks.append(newSock)
-            sockAddr[newSock] = newAddr
-            print("new connection")
-        else:
-            startline, headers, data = http.recvFullMessage(socket)
-            print(data)
-            if startline == b'':
-                socks.remove(socket)
-                if socket in conns:
-                    del conns[socket]
+    def handleDisconnect(self, addr, req):
+        res = checkArgs(req, 'default')
+        if res == True:
+            userID = req['userID']
+            #TODO implement dequeuing and elegant cleanup
+            if userID in conns:
+                del conns[userID]
+                return (200, json.dumps({'success': True}))
             else:
-                print('Request: \n' + startline)
-                page, querydict = http.parseStartLine(startline)
+                return makeError('User not connected')
+        else:
+            return res
 
-                code, reply = handleRequest(sockAddr[socket], data, page)
 
-                httpReply = http.getResponseHead({}, code=str(code), data=reply)
-                http.sendFullMessage(socket, httpReply.encode("utf-8"))
+    def handleCreate(self, req):
+        res = checkArgs(req, 'create')
+        if res == True:
+            dbFile = open('data.txt', 'r')
+            db = json.load(dbFile)
+            if req['userID'] in db['users']:
+                return makeError('User already exists')
+            else:
+                userID = req['userID']
+                vehicle = req['vehicle']
+                name = req['name']
 
-    doLogic()
+                db['users'][userID] = {'vehicle': vehicle, 'name': name}
 
-    end = current_milli_time()
-    delta = (end - start)
-    print("Ticked in {} ms".format(delta))
-time.sleep(max(1 - delta / 1000, 0))
+                new = open('data1.txt', 'w')
+
+                json.dump(db, new, indent=4)
+
+                dbFile.close()
+                new.close()
+
+                os.rename('data.txt', 'data2.txt')
+                os.rename('data1.txt', 'data.txt')
+                os.rename('data2.txt', 'data1.txt')
+
+                return (200, json.dumps({'success': True}))
+        else:
+            return res
+
+    def handleRequest(self, sock, rawReq, page):
+        try:
+            req = parse(rawReq)
+        except Exception as e:
+            print(e)
+            return makeError('Invalid JSON')
+
+        if 'userID' not in req:
+            return makeError('Who is talking to me?')
+        else:
+
+            if page == '/connect':
+                res = checkArgs(req, 'default')
+                if res == True:
+                    conns[req['userID']].handleConnect(req)
+                else:
+                    return res
+            elif page == '/create':
+                res = checkArgs(req, 'create')
+                if res == True:
+                    conns[req['userID']].handleCreate(req)
+                else:
+                    return res
+            else:
+                if req['userID'] not in conns:
+                    return makeError('That user is not connected')
+                else:
+                    if page == '/park':
+                        res = checkArgs(req, 'default')
+                        if res == True:
+                            conns[req['userID']].handlePark(req)
+                        else:
+                            return res
+                    elif page == '/leave':
+                        res = checkArgs(req, 'default')
+                        if res == True:
+                            conns[req['userID']].handleLeave(req)
+                        else:
+                            return res
+                    elif page == '/cancel':
+                        res = checkArgs(req, 'default')
+                        if res == True:
+                            conns[req['userID']].handleCancel(req)
+                        else:
+                            return res
+                    elif page == '/accept':
+                        res = checkArgs(req, 'default')
+                        if res == True:
+                            conns[req['userID']].handleCancel(req)
+                        else:
+                            return res
+                    elif page == '/decline':
+                        res = checkArgs(req, 'default')
+                        if res == True:
+                            conns[req['userID']].handleCancel(req)
+                        else:
+                            return res
+                    else:
+                        return makeError('Invalid command')
+
+
+    # Once all of last second's requests have been processed, do all the logic
+    def doLogic(self):
+        #check for timeouts
+        for user in conns:
+            #TODO make this a more graceful disconnect
+            if current_milli_time() - conns[user].lastActive > 30_000:
+                del conns[user]
+
+
+        #update queue positions
+        for idx, user in enumerate(parkingQueue):
+            user.updateReply(idx)
+        #TODO check for matches
+        return
+
+    def __init__(self):
+        import socket
+        serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serverSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        try:
+            serverSock.bind((HOST, PORT))
+        except socket.error as msg:
+            print('Bind failed. Error Code : ' + str(msg))
+
+        serverSock.listen(10)
+        socks.append(serverSock)
+
+        # Main Loop
+
+        while True:
+            start = current_milli_time()
+
+            readSockets, writeSockets, errorSockets = select.select(socks, [], [], .01)
+
+            for socket in readSockets:
+                if socket == serverSock:
+                    newSock, newAddr = serverSock.accept()
+                    socks.append(newSock)
+                    sockAddr[newSock] = newAddr
+                    print("new connection")
+                else:
+                    startline, headers, data = http.recvFullMessage(socket)
+                    print(data)
+                    if startline == b'':
+                        socks.remove(socket)
+                        if socket in conns:
+                            #TODO more graceful sudden disconnect
+                            del conns[socket]
+                    else:
+                        print('Request: \n' + startline)
+                        page, querydict = http.parseStartLine(startline)
+
+                        self.handleRequest(socket, data, page)
+
+            for socket in writeSockets:
+                if socket != serverSock:
+                    user = sockAddr[socket]
+                    if user.reply != None:
+                        code, reply = user.reply
+
+                        httpReply = http.getResponseHead({}, code=str(code), data=reply)
+                        http.sendFullMessage(socket, httpReply.encode("utf-8"))
+
+            self.doLogic()
+
+            end = current_milli_time()
+            delta = (end - start)
+            print("Ticked in {} ms".format(delta))
+            time.sleep(max(1 - delta / 1000, 0))
+
+s = Server()
--- TakeMySpot Server/user.py
+++ TakeMySpot Server/user.py
@@ -1,25 +1,180 @@
 import json
 import time
+from enum import Enum
+
+
+current_milli_time = lambda: int(round(time.time() * 1000))
+
+successReply = (200, json.dumps({'success' : True}))
+
+
+class State(Enum):
+    START = 6
+    PARKING = 0
+    LEAVING= 1
+    MATCHED_P = 2
+    MATCHED_L = 3
+    EXCHANGING_P = 4
+    EXCHANGING_L = 5
+
+
 
 class User:
+    state = State.START
+    sock = None
+    lastActive = 0
+    server = None
+
+    match = None
+    declinedMatches = []
+
     userID = 0
     vehicle = ""
-    addr = 0
     name = ""
-    lastActive = 0
-    inQueue = False
 
-    def __init__(self, userID, addr):
+    reply = None
+
+    def __init__(self, userID, sock):
         with open('data.txt') as dbFile:
             db = json.load(dbFile)
             if userID in db['users']:
                 self.userID = userID
                 self.vehicle = db['users'][userID]['vehicle']
-                self.addr = addr
+                self.sock = sock
+                self.state = State.START
                 self.name = db['users'][userID]['name']
                 self.lastActive = time.time()
             else:
                 raise "USER NOT FOUND"
 
+    def handleParking(self, argsDict):
+
+        self.lastActive = current_milli_time()
+
+        if self.state != State.START:
+            self.reply = server.makeError('That command is not available for this user state')
+        else:
+            self.state = State.PARKING
+            server.startQueue.append(self)
+            self.reply = successReply
+
+    def handleLeaving(self, argsDict):
+        self.lastActive = current_milli_time()
+
+        if self.state != State.START:
+            self.reply = server.makeError('That command is not available for this user state')
+        else:
+            self.state = State.LEAVING
+            server.leaveQueue.append(self)
+            self.reply = successReply
 
 
+
+    def match(self, matchUser):
+        if self.state == State.LEAVING:
+            self.state = State.MATCHED_L
+            self.match = matchUser
+        elif self.state == State.PARKING:
+            self.state = State.MATCHED_P
+            self.match = matchUser
+
+
+    def handleAccept(self, argsDict):
+        self.lastActive = current_milli_time()
+
+        if self.state == State.MATCHED_P:
+            self.state = State.EXCHANGING_P
+            self.reply = successReply
+
+        elif self.state == State.MATCHED_L:
+            self.state = State.EXCHANGING_L
+            self.reply = successReply
+        else:
+            self.reply = server.makeError('That command is not available for this user state')
+
+
+
+
+    def handleDecline(self, argsDict):
+        self.lastActive = current_milli_time()
+
+        if self.state == State.MATCHED_L:
+            self.state = State.LEAVING
+            self.declinedMatches.append(self.match)
+            self.match = None
+            self.reply = successReply
+
+        #TODO Evaluate whether it is appropriate to have a parked person decline a parker
+        elif self.state == State.MATCHED_P:
+            self.state = State.PARKING
+            self.declinedMatches.append(self.match)
+            self.match = None
+            self.reply = successReply
+
+        else:
+            self.reply = server.makeError('That command is not available for this user state')
+
+
+
+    def handleSuccess(self, argsDict):
+        self.lastActive = current_milli_time()
+
+        if self.state == State.EXCHANGING_P or State.EXCHANGING_L:
+            self.state = State.START
+            self.match = None
+
+            if self.state == State.EXCHANGING_P:
+                server.parkingQueue.remove(self)
+            elif self.state == State.EXCHANGING_L:
+                server.leavingQueue.remove(self)
+            self.reply = successReply
+        else:
+            self.reply = server.makeError('That command is not available for this user state')
+
+
+    def handleFailure(self, argsDict):
+        self.lastActive = current_milli_time()
+
+        if self.state == State.EXCHANGING_P:
+            self.state = State.PARKING
+            self.match = None
+            self.reply = successReply
+
+        elif self.state == State.EXCHANGING_L:
+            self.state = State.LEAVING
+            self.match = None
+            self.reply = successReply
+        else:
+            self.reply = server.makeError('That command is not available for this user state')
+
+
+
+    def handleCancel(self, argsDict):
+        self.lastActive = current_milli_time()
+
+        if self.state == State.LEAVING:
+            self.state = State.START
+            server.leavingQueue.remove(self)
+            self.reply = successReply
+
+        elif self.state == State.PARKING:
+            self.state = State.START
+            server.parkingQueue.remove(self)
+            self.reply = successReply
+
+        elif self.state == State.EXCHANGING_P:
+            self.state = State.START
+            self.match = None
+            server.parkingQueue.remove(self)
+            self.reply = successReply
+
+        elif self.state == State.EXCHANGING_L:
+            self.state = State.START
+            self.match = None
+            server.leavingQueue.remove(self)
+            self.reply = successReply
+        else:
+            self.reply = server.makeError('That command is not available for this user state')
+
+    def updateReply(self, positionInQueue):
+        self.reply = (200, json.dumps({'position': positionInQueue}))
