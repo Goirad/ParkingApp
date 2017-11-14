a8926a47b3acee04aac65bec406118b1ecbb0ef9
Dario Gonzalez <dgonz265@fiu.edu>
2017-11-14 14:24:40 -0500

    fixed update writing to file

--- TakeMySpot Server/test.py
+++ TakeMySpot Server/test.py
@@ -1,7 +1,10 @@
 from dsock import DSock
+import json
 
 sock = DSock()
 
 sock.connect('127.0.0.1', 27182)
-
-sock.send
+mes = json.dumps({'userID' : 'Dario', 'password': 'dario'})
+sock.sendBuffer += ""
+sock.send()
+print(sock.currentMessage)
